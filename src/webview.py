a2253b6c7e0c74df598968a019725ef30e62a107
LTH-8 <LiamHartman2024@gmail.com>
2025-03-03 20:33:36 -0700

    initial window and icon framework

--- src/webview.py
+++ src/webview.py
@@ -1,1 +1,21 @@
+import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt6.QtGui import QIcon
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Spotify Video Sync")
+        self.setWindowIcon(QIcon("resources/logo.png"))
+
+
+    def center_window(self):
+        screen = QApplication.primaryScreen()
+        self.move(self.frameGeometry().moveCenter(screen.availableGeometry.center()))
+
+
+app = QApplication(sys.argv)
+window = Window()
+window.show()
+sys.exit(app.exec())
