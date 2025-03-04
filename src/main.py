0bf620b927ddaaa5cd24026d3d6375c166322fd1
LTH-8 <LiamHartman2024@gmail.com>
2025-03-03 21:51:09 -0700

    dotenv support for spotify client details

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
 import sys
+import os
+
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from PyQt6.QtGui import QIcon
 from PyQt6.uic import loadUi
+from dotenv import load_dotenv
+
+load_dotenv
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+SPOTIFY_REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI")
 
 # Window class
 class Window(QMainWindow):
