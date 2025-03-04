ad1fd9c600d75dbc1ad4ef43690bef6fb0604ed7
LTH-8 <LiamHartman2024@gmail.com>
2025-03-03 21:26:56 -0700

    renaming main.py and adding ui for qtdesigner

--- src/main.py
+++ src/main.py
@@ -2,14 +2,16 @@ import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget
 from PyQt6.QtGui import QIcon
 
+#opening window class
 class Window(QWidget):
     def __init__(self):
         super().__init__()
 
+        #rename opening window title and set app logo
         self.setWindowTitle("Spotify Video Sync")
         self.setWindowIcon(QIcon("resources/logo_large.png"))
 
-
+    #function to resize and center the opening window
     def center_window(self):
         screen = QApplication.primaryScreen()
         self.move(self.frameGeometry().moveCenter(screen.availableGeometry.center()))
